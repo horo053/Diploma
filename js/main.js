6bc62e132d3cc7266b9fe4d4f12c108b4f1d559c
Kristina <yulfi555@gmail.com>
2020-04-04 16:37:06 +0300

    feat: add block cases

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
 
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
 });
